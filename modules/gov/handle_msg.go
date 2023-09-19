cd3daddb555127349f361d567ad252bda6d82fb7
Magic Cat <pusz.monika@gmail.com>
2023-09-19 17:25:34 +0700

    update: proposal tally result on MsgVote

--- modules/gov/handle_msg.go
+++ modules/gov/handle_msg.go
@@ -126,5 +126,11 @@ func (m *Module) handleMsgVote(tx *juno.Tx, msg *govtypes.MsgVote) error {
 
 	vote := types.NewVote(msg.ProposalId, msg.Voter, msg.Option, txTimestamp, tx.Height)
 
-	return m.db.SaveVote(vote)
+	err = m.db.SaveVote(vote)
+	if err != nil {
+		return fmt.Errorf("error while saving vote: %s", err)
+	}
+
+	// update tally result for given proposal
+	return m.UpdateProposalTallyResult(msg.ProposalId, tx.Height)
 }
